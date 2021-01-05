0394128c3f29b5513dc7a1e9a7ba568d7ba72e1c
yelsharawy <yelsharawy20@stuy.edu>
2021-01-05 13:23:45 -0500

    Created MyLinkedList with private nested Node

---
+++ MyLinkedList.java
@@ -0,0 +1,11 @@
+public class MyLinkedList {
+
+    private class Node {
+        private Node prev = null, next = null;
+        private String data;
+    }
+
+    private Node head = null, tail = null;
+    private int size = 0;
+
+}
