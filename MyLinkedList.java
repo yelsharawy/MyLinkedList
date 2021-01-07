27d7a7f7adf1aa9725c0857bb3c78785bae7a2f4
yelsharawy <yelsharawy20@stuy.edu>
2021-01-07 10:40:40 -0500

    Added Node constructors

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,6 +3,21 @@ public class MyLinkedList {
     private class Node {
         private Node prev = null, next = null;
         private String data;
+
+        private Node(String value) {
+            data = value;
+        }
+
+        private Node(Node prev, String value) {
+            this(value);
+            this.prev = prev;
+        }
+
+        private Node(Node prev, String value, Node next) {
+            this(prev, value);
+            this.next = next;
+        }
+
     }
 
     private Node head = null, tail = null;
